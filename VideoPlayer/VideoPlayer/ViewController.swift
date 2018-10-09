84093f6f3f3c8ab65fe0ba5cefe1a5e483c6ea80
JerryCheWei <jerrychang585@gmail.com>
2018-10-09 11:34:26 +0800

    建立project

---
+++ VideoPlayer/VideoPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VideoPlayer
+//
+//  Created by chang-che-wei on 2018/10/9.
+//  Copyright © 2018 chang-che-wei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
